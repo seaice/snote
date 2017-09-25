a8b73b06c07e2bd1d4d1c1a3f374980b11fbfb93
seaice <haibing1458@163.com>
2017-09-25 21:24:42 +0800

    alert

--- src/renderer/components/modal/alert.vue
+++ src/renderer/components/modal/alert.vue
@@ -7,28 +7,45 @@
         >
             <div slot="modal-header">
                 <span>系统错误</span>
-                <a href="#" v-on:click="$refs.alert.hide(false)" class="fr btn-modal-close"  style="-webkit-app-region: no-drag"><i class="fa fa-close" aria-hidden="true"></i></a>
+                <a v-if="close" href="#" v-on:click="$refs.alert.hide(false)" class="fr btn-modal-close"  style="-webkit-app-region: no-drag"><i class="fa fa-close" aria-hidden="true"></i></a>
             </div>
-            <p clas="my-4">
-                {{ msg }}
-            </p>
+            <div class="s-alert" v-bind:class="[state]" v-html="msg"></div>
         </b-modal>
     </div>
 </template>
 <script>
 export default {
+    /*
+        doc：
+        Vue.prototype.$bus.$emit('alert', {msg:'数据异常，请重启笔记！<br>如果重启不能解决问题，请重新安装！',close:false, state:'danger'})
+        or
+        this.$bus.$emit('alert', {msg:'数据异常，请重启笔记！<br>如果重启不能解决问题，请重新安装！',close:false, state:'danger'})
+
+        msg     非必须     显示内容
+        close   非必须     是否显示关闭按钮
+        state   非必须     显示文本样式
+    */ 
+
     data() {
         return {
-            msg : '网络连接超时，请重试！'
+            msg : '网络连接超时，请重试！',
+            close : true,
+            state : 's-alert-primary',
         }
     },
     mounted: function () {
         this.$bus.$on('alert', this.alert)
     },
     methods: {
-        alert (msg) {
-            if(msg != null && msg != '') {
-                this.msg = msg
+        alert (data) {
+            if(data.msg != null && data.msg != '') {
+                this.msg = data.msg
+            }
+            if(data.close === false) {
+                this.close = data.close
+            }
+            if(data.state != null && data.state != '') {
+                this.state = 's-alert-' + data.state
             }
             this.$refs.alert.show()
         }
@@ -36,5 +53,15 @@ export default {
 }
 </script>
 <style>
+#alert .s-alert {
+    font-size: 12px;
+    margin: 0;
+}
+#alert .s-alert-primary {
+    color: #000;
+}
 
+#alert .s-alert-danger {
+    color: #721c24;
+}
 </style>
