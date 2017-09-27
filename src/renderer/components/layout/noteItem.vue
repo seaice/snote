1803be7ac42daa74f1d4c494de35093c260e85bc
duotree <915734542@163.com>
2017-09-28 01:22:05 +0800

    add ueditor
    add ueditor

---
+++ src/renderer/components/layout/noteItem.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul>
+		<slot name="item"
+		v-for="item in items"
+		:text="item.text">
+		<!-- 这里写入备用内容 -->
+		备用呢哦让
+		<li>aaaa</li>
+		</slot>
+    </ul>
+</template>
+
+
+<script type="text/javascript">
+export default {
+    data () {
+        return {
+            items:[]
+        }
+    }
+}
+    
+</script>
---
+++ src/renderer/components/layout/parentNoteItem.vue
@@ -0,0 +1,15 @@
+<noteItem>
+ 	  <template slot="item" scope="props">
+	    <li class="my-fancy-item">{{ props.text }}</li>
+	  </template>
+</noteItem>
+
+<script type="text/javascript">
+    export default {
+    	data () {
+    		return {
+
+    		}
+    	}
+    }
+</script>
