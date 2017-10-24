17c9eb7e295d144c4641b09839769d2da0558c2b
seaice <haibing1458@163.com>
2017-10-24 16:19:01 +0800

    format

--- src/main/index.js
+++ src/main/index.js
@@ -1,32 +1,33 @@
 import { app, BrowserWindow, ipcMain, screen, Tray, nativeImage, Menu } from 'electron'
 /**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
+* Set `__static` path to static files in production
+* https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+*/
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+    global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
 
+let tray = null
 
 function createWindow () {
-  /**
-   * Initial window options
-   */
+/**
+* Initial window options
+*/
 
-  var size = screen.getPrimaryDisplay().workAreaSize
+    var size = screen.getPrimaryDisplay().workAreaSize
 
-  mainWindow = new BrowserWindow({
-    useContentSize: true,
-    width: size.width,
-    height: size.height,
-    minWidth: 800,
-    minHeight : 600,
-    frame: false,
+    mainWindow = new BrowserWindow({
+        useContentSize: true,
+        width: size.width,
+        height: size.height,
+        minWidth: 800,
+        minHeight : 600,
+        frame: false,
     // skipTaskbar: false,
     // fullscreen: true,
     // autoHideMenuBar: true,
@@ -38,88 +39,82 @@ function createWindow () {
     // "webPreferences":{
     //   "webSecurity":false
     // }
-  })
+    })
 
-  mainWindow.loadURL(winURL)
-  // mainWindow.maximize()
+    mainWindow.loadURL(winURL)
+    // mainWindow.maximize()
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 
-  ipcMain.on('note:window:close', function (e) {
-    mainWindow.close()
+    ipcMain.on('note:window:close', function (e) {
+        mainWindow.close()
     // mainWindow.setSkipTaskbar(true)
-  })
+    })
 
-  ipcMain.on('note:window:maximize', function (e) {
-    if (mainWindow.isMaximized()) {
-      mainWindow.unmaximize()
-    } else {
-      mainWindow.maximize()
-    }
-  })
+    ipcMain.on('note:window:maximize', function (e) {
+        if (mainWindow.isMaximized()) {
+          mainWindow.unmaximize()
+      } else {
+          mainWindow.maximize()
+      }
+    })
 
-  ipcMain.on('note:window:minimize', function (e) {
-    mainWindow.minimize()
-  })
+    ipcMain.on('note:window:minimize', function (e) {
+        mainWindow.minimize()
+    })
 
-  // const iconName = process.platform === 'win32'
-  let tray = null
-  // tray = new Tray('../renderer/assets/img/tray.png')
-  // tray = new Tray(__dirname + '../assets/img/tray.png')
-  // 
-  // 
-  // 
-  /*
-const path = require('path')
-const trayIcon = path.join(__dirname, 'tray.png');
-const nimage = nativeImage.createFromPath(trayIcon);
+    // const iconName = process.platform === 'win32'
 
-  // tray = new Tray(__dirname + 'tray.png')
-  tray = new Tray(nimage)
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
-*/
+
+    const path = require('path')
+    const trayIcon = path.join(__dirname, '../renderer/assets/ico/tray.png');
+    const nimage = nativeImage.createFromPath(trayIcon);
+
+    // tray = new Tray(__dirname + 'tray.png')
+    tray = new Tray(nimage)
+    const contextMenu = Menu.buildFromTemplate([
+        {label: 'Item1', type: 'radio'},
+        {label: 'Item2', type: 'radio'},
+        {label: 'Item3', type: 'radio', checked: true},
+        {label: 'Item4', type: 'radio'}
+        ])
+    tray.setToolTip('SNote记录每一天的成长')
+    tray.setContextMenu(contextMenu)
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 
 /**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
+* Auto Updater
+*
+* Uncomment the following code below and install `electron-updater` to
+* support auto updating. Code Signing with a valid certificate is required.
+* https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+*/
 
 /*
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
+autoUpdater.quitAndInstall()
 })
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
+*/
